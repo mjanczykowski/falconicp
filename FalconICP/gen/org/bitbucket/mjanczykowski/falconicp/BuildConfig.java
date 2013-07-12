2a30a0ecee8f6e6761835a689c7b9a7f3351588f
Michał Janczykowski <mjanczykowski@gmail.com>
2013-07-12 02:51:22 +0200

    first commit

---
+++ FalconICP/gen/org/bitbucket/mjanczykowski/falconicp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.bitbucket.mjanczykowski.falconicp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
