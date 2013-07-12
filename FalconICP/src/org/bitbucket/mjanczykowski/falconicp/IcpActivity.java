e75b0baa887053f9adc6be8de13bfa19fd458314
Michał Janczykowski <mjanczykowski@gmail.com>
2013-07-12 02:57:35 +0200

    fullscreen mode

--- FalconICP/src/org/bitbucket/mjanczykowski/falconicp/IcpActivity.java
+++ FalconICP/src/org/bitbucket/mjanczykowski/falconicp/IcpActivity.java
@@ -3,12 +3,18 @@ package org.bitbucket.mjanczykowski.falconicp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class IcpActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.activity_icp);
     }
 
