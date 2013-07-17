283445550fa697190004ef9254e4262bad5d7960
Michał Janczykowski <mjanczykowski@gmail.com>
2013-07-18 00:09:19 +0200

    DCS + DriftWarn

--- FalconICP/src/org/bitbucket/mjanczykowski/falconicp/DCSView.java
+++ FalconICP/src/org/bitbucket/mjanczykowski/falconicp/DCSView.java
@@ -2,23 +2,103 @@ package org.bitbucket.mjanczykowski.falconicp;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 
 public class DCSView extends ImageView {
-
-	public DCSView(Context context) {
-		super(context);
-		// TODO Auto-generated constructor stub
-	}
+	
+	/* Indicates current direction of the DCS stick */
+	private State state = State.CENTER;
+	
+	/* Coordinates registered on ACTION_DOWN */
+	private float actionDownX;
+	private float actionDownY;
 
 	public DCSView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
 	}
 
-	public DCSView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		float eventX = event.getX();
+		float eventY = event.getY();
+		
+		switch(event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				actionDownX = eventX;
+				actionDownY = eventY;
+				return true;
+			case MotionEvent.ACTION_MOVE:
+				float dx = eventX - actionDownX;
+				float dy = eventY - actionDownY;
+				float absDx = Math.abs(dx);
+				float absDy = Math.abs(dy);
+				
+				if(absDx < 40 && absDy < 40) {
+					setState(State.CENTER);
+					return true;
+				}
+				if(absDx > absDy) {
+					if(dx > 0) {
+						setState(State.RIGHT);
+					}
+					else {
+						setState(State.LEFT);
+					}
+					return true;
+				}
+				if(dy > 0) {
+					setState(State.DOWN);
+				}
+				else {
+					setState(State.UP);
+				}
+				return true;
+			case MotionEvent.ACTION_UP:
+				if(state != State.CENTER) {
+					Log.i("DCS action", state.toString());
+					//TO-DO run action if state != center
+					setState(State.CENTER);
+				}
+				return true;
+			default:
+				return false;
+		}
 	}
-
+	
+	/**
+	 * Updates DCS switch state and change image if necessary   
+	 * @param s New DCS switch state
+	 */
+	private void setState(State s) {
+		if(state != s)
+		{
+			state = s;
+			int resId;
+			switch(s) {
+				case LEFT:
+					resId = R.drawable.dcs_left;
+					break;
+				case RIGHT:
+					resId = R.drawable.dcs_right;
+					break;
+				case UP:
+					resId = R.drawable.dcs_up;
+					break;
+				case DOWN:
+					resId = R.drawable.dcs_down;
+					break;
+				default:
+					resId = R.drawable.dcs_center;
+			}
+			setImageResource(resId);
+		}
+	}
+	
+	/** States of DCS switch - direction of the stick */
+	private enum State {
+		CENTER, LEFT, UP, RIGHT, DOWN;
+	}
 }
---
+++ FalconICP/src/org/bitbucket/mjanczykowski/falconicp/DriftWarnSwitch.java
@@ -0,0 +1,100 @@
+package org.bitbucket.mjanczykowski.falconicp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+public class DriftWarnSwitch extends ImageView {
+	
+	/** Indicates position of the switch */
+	private State state = State.NORM;
+	private State previousState = State.NORM;
+	
+	/* Coordinates registered on ACTION_DOWN */
+	private float actionDownY;
+
+	public DriftWarnSwitch(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		float eventY = event.getY();
+		switch(event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				actionDownY = eventY;
+				previousState = state;
+				return true;
+			case MotionEvent.ACTION_MOVE:
+				float dy = eventY - actionDownY;
+				float absDy = Math.abs(dy);
+				if(absDy < 40) {
+					setState(previousState);
+					return true;
+				}
+				if(dy > 0) {
+					setState(State.valueOf(previousState.getValue() - 1));
+				}
+				else {
+					setState(State.valueOf(previousState.getValue() + 1));
+				}
+				return true;
+			case MotionEvent.ACTION_UP:
+				if(state != previousState) {
+					Log.i("DriftWarn action", state.toString());
+					// perform action
+					if(state == State.WARN_RESET)
+						setState(State.NORM);
+				}
+				return true;
+			default:
+				return false;
+		}
+	}
+	
+	public void setState(State s) {
+		if(state != s) {
+			state = s;
+			int resId;
+			switch(s) {
+				case DRIFT_CO:
+					resId = R.drawable.driftwarn_up;
+					break;
+				case NORM:
+					resId = R.drawable.driftwarn_center;
+					break;
+				case WARN_RESET:
+				default:
+					resId = R.drawable.driftwarn_down;
+			}
+			setImageResource(resId);
+		}
+	}
+
+	/** Position of the switch */
+	public enum State {
+		DRIFT_CO(1), NORM(0), WARN_RESET(-1);
+		
+		private final int value;
+		
+		private State(int value) {
+			this.value = value;
+		}
+		
+		public int getValue() {
+			return value;
+		}
+		
+		public static State valueOf(int i) {
+			if(i == 0)
+				return State.NORM;
+			else if(i > 0)
+				return State.DRIFT_CO;
+			else
+				return State.WARN_RESET;
+		}
+	}
+}
